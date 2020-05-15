65e8ae293e6a8fe814dee1fcf21b4c6a6075b2b5
justinmajetich <justinmajetich@gmail.com>
2020-05-15 22:40:57 +0000

    Fix checked amenity list and checkbox styling

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,23 +1,22 @@
 window.onload = () => {
-
   const checked = {};
 
-  $('INPUT:checkbox').click(function() {
+  $('INPUT:checkbox').click(function () {
     console.log('Inside input block');
-    if($(this).prop('checked') == true){
+    if ($(this).prop('checked') === true) {
       checked[$(this).attr('data-id')] = $(this).attr('data-name');
-    }
-    else {
+    } else {
       delete checked[($(this).attr('data-id'))];
     }
-    const names = $.map(checked, (value, key) => { return value });
+    const names = $.map(checked, (value, key) => { return value; });
     let formattedString = '';
     for (let i = 0; i < names.length; i++) {
       formattedString += names[i];
       if (i < names.length - 1) {
-        formattedString += ',&nbsp;';
+        formattedString += ', ';
       }
     }
-	$('DIV.amenities H4').html(formattedString);
+    formattedString += '&nbsp;';
+    $('DIV.amenities H4').html(formattedString);
   });
 };
