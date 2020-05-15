751f1a01874c8942610a147cd6ca6e043e751659
justinmajetich <justinmajetich@gmail.com>
2020-05-15 20:14:56 +0000

    Edited JQuery script to display list of checked amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,23 @@
-const checkList = [];
-$('INPUT:checkbox').click(function() {
-  if($(this).prop('checked') == true){
-    checkList.append($(this).prop('amenity_id'));
-  }
-  else {
-    checkList.pop($(this).prop('amenity_id'));
-  }
-});
+window.onload = () => {
 
-$('DIV.amenities H4').text(checkList);
+  const checked = {};
+
+  $('INPUT:checkbox').click(function() {
+    console.log('Inside input block');
+    if($(this).prop('checked') == true){
+      checked[$(this).attr('data-id')] = $(this).attr('data-name');
+    }
+    else {
+      delete checked[($(this).attr('data-id'))];
+    }
+    const names = $.map(checked, (value, key) => { return value });
+    let formattedString = '';
+    for (let i = 0; i < names.length; i++) {
+      formattedString += names[i];
+      if (i < names.length - 1) {
+        formattedString += ',&nbsp;';
+      }
+    }
+	$('DIV.amenities H4').html(formattedString);
+  });
+};
