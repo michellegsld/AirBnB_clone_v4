70bc0264eb0266df495655ffa03a1db675f2d9ca
justinmajetich <justinmajetich@gmail.com>
2020-05-16 00:38:52 +0000

    Fix styling for api status indicator

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,28 +1,27 @@
 window.onload = () => {
-
   const checked = {};
 
-  $('INPUT:checkbox').click(function() {
+  $('INPUT:checkbox').click(function () {
     console.log('Inside input block');
-    if($(this).prop('checked') == true){
+    if ($(this).prop('checked') === true) {
       checked[$(this).attr('data-id')] = $(this).attr('data-name');
-    }
-    else {
+    } else {
       delete checked[($(this).attr('data-id'))];
     }
-    const names = $.map(checked, (value, key) => { return value });
+    const names = $.map(checked, (value, key) => { return value; });
     let formattedString = '';
     for (let i = 0; i < names.length; i++) {
       formattedString += names[i];
       if (i < names.length - 1) {
-        formattedString += ',&nbsp;';
+        formattedString += ', ';
       }
     }
-  $('DIV.amenities H4').html(formattedString);
+    formattedString += '&nbsp;';
+    $('DIV.amenities H4').html(formattedString);
   });
 
-  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function(data) {
-    if (data['status'] == 'OK') {
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
       $('DIV#api_status').removeClass('available');
